480a0714a4d8d28c500d121a81cf9cf145c1eb57
Gavin Shirey <gavintheorca1@gmail.com>
2022-03-24 19:36:49 -0400

    Started work on updating the System View
    Added a new prefab

--- Assets/Scripts/UI Scripts/SystemView.cs
+++ Assets/Scripts/UI Scripts/SystemView.cs
@@ -9,17 +9,23 @@ public class SystemView : MonoBehaviour
     public TextMeshProUGUI pop;
     public TextMeshProUGUI species;
     private int totalPop;
+    [SerializeField] GameObject SpeciesCircle;
+    [SerializeField] Canvas canvas;
 
     private void Start()
     {
-        
+        RectTransform rect = GetComponent<RectTransform>();
 
         string newtext = "";
         totalPop = 0;
         foreach (KeyValuePair<string, int> kv in GameManager.Instance.getCurrentWorld().getAllSpeciesPopulation())
         {
-            newtext += kv.Key + ": " + kv.Value + "\n";
-            totalPop += kv.Value;
+            //newtext += kv.Key + ": " + kv.Value + "\n";
+            //totalPop += kv.Value;
+            GameObject s = Instantiate(SpeciesCircle, new Vector3(Random.Range(0, rect.rect.width), Random.Range(0, rect.rect.height), 0), new Quaternion(), canvas.transform);
+            s.transform.localScale = new Vector3(2, 2, 1);
+            s.transform.Find("Species").GetComponent<TextMeshProUGUI>().SetText(kv.Key);
+            s.transform.Find("Population").GetComponent<TextMeshProUGUI>().SetText(kv.Value.ToString());
         }
 
         species.SetText(newtext);
