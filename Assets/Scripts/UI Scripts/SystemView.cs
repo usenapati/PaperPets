1bb98d79b644f3a795393ecaa4203a90bdc8398a
Gavin Shirey <gavintheorca1@gmail.com>
2022-03-25 13:55:48 -0400

    Trying to make System View look better
    Made the circles spawn within bounds, working on grid texture mapping

--- Assets/Scripts/UI Scripts/SystemView.cs
+++ Assets/Scripts/UI Scripts/SystemView.cs
@@ -15,14 +15,15 @@ public class SystemView : MonoBehaviour
     private void Start()
     {
         RectTransform rect = GetComponent<RectTransform>();
+        RectTransform circleSize = SpeciesCircle.GetComponent<RectTransform>();
 
         string newtext = "";
         totalPop = 0;
         foreach (KeyValuePair<string, int> kv in GameManager.Instance.getCurrentWorld().getAllSpeciesPopulation())
         {
             //newtext += kv.Key + ": " + kv.Value + "\n";
-            //totalPop += kv.Value;
-            GameObject s = Instantiate(SpeciesCircle, new Vector3(Random.Range(0, rect.rect.width), Random.Range(0, rect.rect.height), 0), new Quaternion(), canvas.transform);
+            totalPop += kv.Value;
+            GameObject s = Instantiate(SpeciesCircle, new Vector3(Random.Range(circleSize.rect.width, rect.rect.width - circleSize.rect.width), Random.Range(circleSize.rect.height, rect.rect.height - circleSize.rect.height), 0), new Quaternion(), canvas.transform);
             s.transform.localScale = new Vector3(2, 2, 1);
             s.transform.Find("Species").GetComponent<TextMeshProUGUI>().SetText(kv.Key);
             s.transform.Find("Population").GetComponent<TextMeshProUGUI>().SetText(kv.Value.ToString());
