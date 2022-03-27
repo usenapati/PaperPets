a8bb71c686e36b416c139d053ba6f4e49f6f3316
Gavin Shirey <gavintheorca1@gmail.com>
2022-03-27 01:57:46 -0400

    Made Species Circles better
    Also fixed scroll speed, added connecting curves.

--- Assets/Scripts/UI Scripts/SystemView.cs
+++ Assets/Scripts/UI Scripts/SystemView.cs
@@ -10,26 +10,89 @@ public class SystemView : MonoBehaviour
     [SerializeField] TextMeshProUGUI pop;
     [SerializeField] Renderer rect;
     [SerializeField] GameObject SpeciesCircle;
+    [SerializeField] GameObject FoodLinePrefab;
+    [SerializeField] GameObject HabitatLinePrefab;
+    [SerializeField] float minScale;
     [SerializeField] float startingScale;
 
+    Dictionary<string, Species> organisms;
+    Dictionary<string, GameObject> circles;
+
     private void Start()
+    {
+        organisms = new Dictionary<string, Species>();
+        circles = new Dictionary<string, GameObject>();
+
+        totalPop = 0;
+        foreach (Species s in GameManager.Instance.getCurrentWorld().getAllSpecies())
+        {
+            totalPop += s.population;
+            organisms.Add(s.name, s);
+        }
+
+        drawCircles();
+        drawLines();
+
+        pop.SetText(totalPop.ToString());
+        print("test");
+    }
+
+    private void drawCircles()
     {
         Vector3 dim = rect.bounds.size;
         Vector3 circleSize = SpeciesCircle.GetComponent<Renderer>().bounds.size;
 
-        totalPop = 0;
-        foreach (KeyValuePair<string, int> kv in GameManager.Instance.getCurrentWorld().getAllSpeciesPopulation())
+        foreach (Species sp in organisms.Values)
         {
-            totalPop += kv.Value;
+            float finalScale = Mathf.Max(minScale, startingScale * sp.population / totalPop);
             GameObject s = Instantiate(SpeciesCircle, new Vector3(Random.Range(circleSize.x, dim.x - circleSize.x), -1, Random.Range(circleSize.z, dim.z - circleSize.z)), new Quaternion(), transform);
-            s.transform.localScale = new Vector3(startingScale, startingScale, 1);
-            s.transform.position += new Vector3(dim.x / -2, 0, dim.z / -2);
-            s.transform.position += new Vector3(0, 0, 1);
-            s.transform.Find("Species").GetComponent<TextMeshPro>().SetText(kv.Key + "\n" + kv.Value);
-        }
 
-        pop.SetText(totalPop.ToString());
-        print("test");
+            s.transform.localScale = new Vector3(finalScale, finalScale, 1);
+            s.transform.position += new Vector3(dim.x / -2, 0, dim.z / -2);
+            s.transform.position += new Vector3(0, 10, 0);
+
+            s.transform.Find("Species").GetComponent<TextMeshPro>().SetText(sp.name + "\n" + sp.population);
+            SpeciesVisualData sd = Resources.Load("Visuals/" + sp.name.Replace(" ", "")) as SpeciesVisualData;
+            s.GetComponent<SpriteRenderer>().color = sd == null ? Color.black : sd.speciesColor;
+            circles.Add(sp.name, s);
+        }
+    }
+
+    void DrawQuadraticBezierCurve(LineRenderer lr, Vector3 start, Vector3 mid, Vector3 end)
+    {
+        lr.positionCount = 200;
+        float t = 0f;
+        Vector3 B = new Vector3(0, 0, 0);
+        for (int i = 0; i < lr.positionCount; i++)
+        {
+            B = (1 - t) * (1 - t) * start + 2 * (1 - t) * t * mid + t * t * end;
+            lr.SetPosition(i, B);
+            t += (1 / (float) lr.positionCount);
+        }
+    }
+
+    private void drawLines()
+    {
+        foreach (Species s in organisms.Values)
+        {
+            foreach (Species food in s.outgoingFood)
+            {
+                GameObject line = Instantiate(FoodLinePrefab, transform);
+                LineRenderer lr = line.GetComponent<LineRenderer>();
+
+                Vector3 mid = new Vector3(circles[food.name].transform.position.x, .25f, circles[s.name].transform.position.z);
+                DrawQuadraticBezierCurve(lr, circles[s.name].transform.position, mid, circles[food.name].transform.position);
+            }
+
+            foreach (Species habitat in s.outgoingHabitat)
+            {
+                GameObject line = Instantiate(HabitatLinePrefab, transform);
+                LineRenderer lr = line.GetComponent<LineRenderer>();
+
+                Vector3 mid = new Vector3(circles[s.name].transform.position.x, .25f, circles[habitat.name].transform.position.z);
+                DrawQuadraticBezierCurve(lr, circles[s.name].transform.position, mid, circles[habitat.name].transform.position);
+            }
+        }
     }
 
 }
--- Assets/Scripts/UI Scripts/SystemViewCameraControls.cs
+++ Assets/Scripts/UI Scripts/SystemViewCameraControls.cs
@@ -7,19 +7,29 @@ public class SystemViewCameraControls : MonoBehaviour
 {
 
     [SerializeField] Collider bounds;
+    [SerializeField] Camera cam;
+    [SerializeField] float initMoveSpeed;
+    [SerializeField] float zoomSpeed;
 
     float UDVal;
     float LRVal;
     float ZoomVal;
     float camWidth;
     float camHeight;
+    float maxZoom;
+    float initZoom;
+    float moveSpeed;
     Vector3 translation = new Vector3();
     Vector3 targetPos = new Vector3();
+    float targetZoom;
 
     private void Awake()
     {
         camWidth = Camera.main.orthographicSize * Screen.width / Screen.height;
         camHeight = Camera.main.orthographicSize;
+        maxZoom = (Screen.height * bounds.bounds.extents.x) / Screen.width;
+        initZoom = Camera.main.orthographicSize;
+        moveSpeed = initMoveSpeed;
         Debug.Log(camWidth + " " + camHeight);
     }
 
@@ -32,29 +42,20 @@ public class SystemViewCameraControls : MonoBehaviour
 
         if (UDVal > 0)
         {
-            translation.z = 1;
+            translation.z = moveSpeed;
         }
         else if (UDVal < 0)
         {
-            translation.z = -1; ;
+            translation.z = -moveSpeed;
         }
 
         if (LRVal > 0)
         {
-            translation.x = 1;
+            translation.x = moveSpeed;
         }
         else if (LRVal < 0)
         {
-            translation.x = -1;
-        }
-
-        if (ZoomVal > 0)
-        {
-
-        }
-        else if (ZoomVal < 0)
-        {
-
+            translation.x = -moveSpeed;
         }
 
         targetPos = transform.position + translation;
@@ -62,6 +63,26 @@ public class SystemViewCameraControls : MonoBehaviour
         targetPos.z = Mathf.Clamp(targetPos.z, bounds.bounds.min.z + camHeight, bounds.bounds.max.z - camHeight);
         transform.position = targetPos;
 
+    }
+
+    private void Update()
+    {
+        if (ZoomVal > 0)
+        {
+            targetZoom = cam.orthographicSize - zoomSpeed;
+            cam.orthographicSize = Mathf.Clamp(targetZoom, 1, maxZoom);
+            camWidth = Camera.main.orthographicSize * Screen.width / Screen.height;
+            camHeight = Camera.main.orthographicSize;
+            moveSpeed = initMoveSpeed * cam.orthographicSize / initZoom;
+        }
+        else if (ZoomVal < 0)
+        {
+            targetZoom = cam.orthographicSize + zoomSpeed;
+            cam.orthographicSize = Mathf.Clamp(targetZoom, 1, maxZoom);
+            camWidth = Camera.main.orthographicSize * Screen.width / Screen.height;
+            camHeight = Camera.main.orthographicSize;
+            moveSpeed = initMoveSpeed * cam.orthographicSize / initZoom;
+        }
     }
 
     public void OnUD(InputAction.CallbackContext context)
@@ -72,6 +93,11 @@ public class SystemViewCameraControls : MonoBehaviour
     public void OnLR(InputAction.CallbackContext context)
     {
         LRVal = context.ReadValue<float>();
+    }
+
+    public void OnZoom(InputAction.CallbackContext context)
+    {
+        ZoomVal = context.ReadValue<Vector2>().y;
     }
 
 }
