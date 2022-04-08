0a9a20fd678d75f28a29df616fba601927dc34da
Gavin Shirey <gavintheorca1@gmail.com>
2022-04-08 13:48:37 -0400

    Made System View easier to access, little minimap on main page
    Need to make it more clickable

--- Assets/Scripts/UI Scripts/LoadMinimap.cs
+++ Assets/Scripts/UI Scripts/LoadMinimap.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class LoadMinimap : MonoBehaviour
+public class LoadMinimap : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] RawImage r;
+    [SerializeField] SceneController sceneManager;
     void Start()
     {
         if (SaveMinimapTexture.Instance != null) r.texture = SaveMinimapTexture.Instance.t;
+    }
+
+    public void OnPointerClick(PointerEventData e)
+    {
+        sceneManager.ChangeScene("SystemView");
     }
 }
