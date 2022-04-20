4d457de5af630520a3a4a6a0fc525f6d748c9591
Gavin Shirey <gavintheorca1@gmail.com>
2022-04-19 23:01:39 -0400

    Got Audio stems working with bool array
    Need to link the bools changing to the world

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [SerializeField] AudioSource baseAudioPlayer;
+    [SerializeField] AudioClip baseTrack;
+    [SerializeField] AudioClip loopStem1;
+    [SerializeField] AudioClip loopStem2;
+    [SerializeField] AudioClip loopStem3;
+    [SerializeField] float loopTimeSec;
+    [SerializeField] float stemInterval;
+    float accumulator;
+
+    AudioSource track1;
+    AudioSource track2;
+    AudioSource track3;
+    [SerializeField] float[,] stemIntervals = {
+        { 18.300f, 36.891f, 55.187f, 0 },
+        { 8.969f, 45.511f, 54.953f, 0 },
+        { 18.199f, 36.528f, 54.722f, 0 } };
+    [SerializeField] bool[] stemsPlaying = new bool[3];
+
+    private static AudioManager _instance;
+    public static AudioManager Instance { get { return _instance; } }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        else
+        {
+            _instance = this;
+        }
+        DontDestroyOnLoad(this);
+        baseAudioPlayer.clip = baseTrack;
+        //baseAudioPlayer.time = 60;
+        baseAudioPlayer.Play();
+
+        stemIntervals[0, 3] = loopTimeSec;
+        stemIntervals[1, 3] = loopTimeSec;
+        stemIntervals[2, 3] = loopTimeSec;
+
+        track1 = gameObject.AddComponent<AudioSource>();
+        track1.clip = loopStem1;
+        track2 = gameObject.AddComponent<AudioSource>();
+        track2.clip = loopStem2;
+        track3 = gameObject.AddComponent<AudioSource>();
+        track3.clip = loopStem3;
+        StartCoroutine(ManageClip(track1, 0));
+        StartCoroutine(ManageClip(track2, 1));
+        StartCoroutine(ManageClip(track3, 2));
+    }
+
+    IEnumerator ManageClip(AudioSource clip, int track)
+    {
+
+        for (int i = 0; true; i = (i + 1) % 4)
+        {
+            //i = Mathf.Clamp(i, 0, loopTimeSec);
+            if (i >= loopTimeSec) i = 0;
+            if (stemsPlaying[track])
+            {
+                if (!clip.isPlaying)
+                {
+                    //clip.Stop();
+                    clip.time = baseAudioPlayer.time;
+                    clip.Play();
+                }
+                /*if (i == 0)
+                {
+                    clip.time = 0; yield return new WaitForSeconds(stemIntervals[track, i] - .1f);
+                }*/
+                yield return new WaitForSeconds(stemIntervals[track, i] - baseAudioPlayer.time - .1f);
+            }
+            else
+            {
+                clip.Stop();
+                yield return new WaitForSeconds(stemIntervals[track, i] - baseAudioPlayer.time - .1f);
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        accumulator += Time.deltaTime;
+        if (accumulator >= loopTimeSec)
+        {
+            baseAudioPlayer.time = 0;
+            track1.time = 0;
+            track2.time = 0;
+            track3.time = 0;
+            accumulator = 0;
+        }
+    }
+}
